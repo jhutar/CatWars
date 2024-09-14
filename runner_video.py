496c904f9c3ec3b3679bd44a39cd6533e427ec41
Jan Hutar <jhutar@seznam.cz>
2024-09-14 08:41:34 +0200

    Refactor classes to contain dispatch()

--- runner_video.py
+++ runner_video.py
@@ -4,40 +4,82 @@ import pygame
 import sys
 import random
 
+class Options():
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+
+class Game(pygame.sprite.Sprite):
+    def __init__(self, options):
+        self.options = options
+        self.font = pygame.font.Font("font/Pixeltype.ttf", 50)
+        self.score = 0
+
+    def draw(self, screen):
+        score_surf = self.font.render(f"Score: {self.score}", False, (200, 50, 100))
+        score_rect = score_surf.get_rect(center=(400, 50))
+        screen.blit(score_surf, score_rect)
+
+    def dispatch(self, event):
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            print(f"SCORE: {self.score}")   # TODO: Deduplicate
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()
+                print(f"SCORE: {self.score}")   # TODO: Deduplicate
+                sys.exit()
+
+    def update(self):
+        pass
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
+
+        # Refference to game data
+        self.game = game
+
+        # Sprite necessities
         self.image = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = (-30, 300)
+
+        # Entity properties
         self.health = 10
 
-    def update(self):
-        self.move()
+    def dispatch(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.demage()
 
-    def move(self):
-        self.rect.x += 1
+    def update(self):
+        self.rect.x += 5
+
+        if self.rect.x > self.game.options.width:
+            self.kill()
+            self.game.score -= 3
 
     def demage(self):
         power = random.randint(2, 6)
         self.health -= power
         if self.health <= 0:
             self.kill()
-            return 1
-        else:
-            return 0
+            self.game.score += 1
+
+options = Options()
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((options.width, options.height))
 pygame.display.set_caption("CatWars")
 clock = pygame.time.Clock()
-score = 0
-game_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 ###bg_music = pygame.mixer.Sound("audio/music.wav")
 ###bg_music.play(loops=-1)
 
 # Groups
+game = Game(options)
 enemies_group = pygame.sprite.Group()
 
 # Timers
@@ -49,40 +91,21 @@ pygame.time.set_timer(spawn_timer, 1000)
 # Main loop
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            print(f"SCORE: {score}")   # TODO: Deduplicate
-            sys.exit()
+        game.dispatch(event)
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                pygame.quit()
-                print(f"SCORE: {score}")   # TODO: Deduplicate
-                sys.exit()
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for enemy in enemies_group:
-                if enemy.rect.collidepoint(event.pos):
-                    result = enemy.demage()
-                    score += result
+        for enemy in enemies_group:
+            enemy.dispatch(event)
 
         ###if event.type == animation_timer:
         ###    pass
 
         if event.type == spawn_timer:
-            enemies_group.add(Enemy())
+            enemies_group.add(Enemy(game))
 
-    # Enemies won?
-    for enemy in enemies_group:
-        if enemy.rect.x > 800:
-            enemy.kill()
-            score -= 3
 
     screen.fill("darkgreen")
 
-    score_surf = game_font.render(f"Score: {score}", False, (200, 50, 100))
-    score_rect = score_surf.get_rect(center=(400, 50))
-    screen.blit(score_surf, score_rect)
+    game.draw(screen)
 
     enemies_group.draw(screen)
     enemies_group.update()
