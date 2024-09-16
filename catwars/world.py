f8a2e7d7087208c6851ead94f731a3dc4ae910d4
Jan Hutar <jhutar@redhat.com>
2024-09-16 21:40:42 +0200

    feat: Convert map to group of sprites to allow colision detection

--- catwars/world.py
+++ catwars/world.py
@@ -1,17 +1,28 @@
+import pygame
 import pytmx
 
-class World():
+class GroundTile(pygame.sprite.Sprite):
+    """Generic ground sprite class for map tiles."""
+    def __init__(self, image, topleft):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = topleft
+
+
+class World(pygame.sprite.Group):
     def __init__(self, level):
+        super().__init__()
+
         self.tmxdata = pytmx.util_pygame.load_pygame(level)
         self.world_size = (
             self.tmxdata.width * self.tmxdata.tilewidth,
             self.tmxdata.height * self.tmxdata.tileheight,
         )
 
-    def draw(self, screen):
         for layer in self.tmxdata:
             if layer.name == "ground":
                 for tile in layer.tiles():
                     x_top_left = tile[0] * self.tmxdata.tilewidth
                     y_top_left = tile[1] * self.tmxdata.tileheight
-                    screen.blit(tile[2], (x_top_left, y_top_left))
+                    self.add(GroundTile(tile[2], (x_top_left, y_top_left)))
