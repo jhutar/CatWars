8e2cf3d6763c269a785e637ddc49d2ad2a37b8a6
Jan Hutar <jhutar@seznam.cz>
2024-09-19 13:24:15 +0200

    fix: Add forgotten files for first bits of UI

---
+++ catwars/buttons.py
@@ -0,0 +1,44 @@
+import pygame
+import os.path
+
+import catwars.generics
+
+class ButtonsGroup(catwars.generics.GroupWithDispatch):
+    """Generic group of UI buttons."""
+    def __init__(self, game):
+        super().__init__()
+        
+        self.game = game
+
+        self.build_button = Button(self.game, (0, 500))
+        self.add(self.build_button)
+
+class Button(pygame.sprite.Sprite):
+    """Generic button sprite class."""
+    def __init__(self, game, topleft):
+        super().__init__()
+
+        self.game = game
+        self.active = False
+
+        # Sprite necessities
+        img_path_idle = os.path.join(self.game.assets_dir, "graphics/ui/button-build-idle.png")
+        print(">>>", img_path_idle)
+        i = pygame.image.load(img_path_idle).convert_alpha()
+        img_path_active = os.path.join(self.game.assets_dir, "graphics/ui/button-build-active.png")
+        self.images = {
+            False: pygame.image.load(img_path_idle).convert_alpha(),
+            True: pygame.image.load(img_path_active).convert_alpha(),
+        }
+        self.image = self.images[self.active]
+        self.rect = self.image.get_rect()
+        self.rect.topleft = topleft
+
+    def dispatch(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+                return True
+
+    def update(self):
+        self.image = self.images[self.active]
