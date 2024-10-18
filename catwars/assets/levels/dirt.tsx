2bab56ed488e6357535c32a0acd95d3227554543
Jan Hutar <jhutar@seznam.cz>
2024-10-18 21:33:42 +0200

    feat: New map and configured terain brush

--- catwars/assets/levels/dirt.tsx
+++ catwars/assets/levels/dirt.tsx
@@ -101,4 +101,25 @@
    <property name="can_walk" type="bool" value="true"/>
   </properties>
  </tile>
+ <wangsets>
+  <wangset name="dirt" type="corner" tile="10">
+   <wangcolor name="dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="4" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="6" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="13" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="15" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
