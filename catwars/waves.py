c7d2173fd886ebde75fc15b22bacc027e06ca6f2
Jan Hutar <jhutar@seznam.cz>
2025-06-07 22:34:47 +0200

    feat: Pass game instead of level when creating Waves

--- catwars/waves.py
+++ catwars/waves.py
@@ -3,16 +3,18 @@ import os
 import json
 
 import catwars.enemies
+import catwars.helpers
 
 
 class Waves:
     """Waves of enemies in level."""
 
-    def __init__(self, level, data_path_rel):
-        self.level = level
+    def __init__(self, game, data_path_rel):
+        self.game = game
+        self.options = catwars.helpers.Options()
 
         # Load waves data
-        data_path = os.path.join(self.level.options.assets_dir, data_path_rel)
+        data_path = os.path.join(self.options.assets_dir, data_path_rel)
         with open(data_path, "r") as fd:
             self._data = json.load(fd)
         self._index = 0
@@ -36,16 +38,16 @@ class Waves:
 
     def handle_wave(self):
         # Handle case during init when we are initializing group
-        enemies_group_initializing = "enemies_group" not in dir(self.level)
+        enemies_group_initializing = "enemies_group" not in dir(self.game)
 
         # No more waves available
         no_more_waves = self._index >= len(self._data)
 
-        # If there are no enemies and there is no more waves, quit
+        # If there are no enemies and there are no more waves, quit
         if not enemies_group_initializing:
-            if len(self.level.enemies_group) == 0:
+            if len(self.game.enemies_group) == 0:
                 if no_more_waves:
-                    self.level.is_active = False
+                    self.game.is_active = False
                     return
 
         # If we are still running and there are no more waves, just sleep
@@ -60,7 +62,7 @@ class Waves:
             pygame.time.set_timer(self.wave_timer, config["delay"] * 1000, loops=1)
             self.next_wave_in = config["delay"]
             self.next_wave_started = pygame.time.get_ticks()
-            self.level.logger.debug(
+            self.game.logger.debug(
                 f"Starting idle wave {self._index} in {config['delay']} seconds"
             )
             self._index += 1
@@ -69,25 +71,25 @@ class Waves:
                 self.burst_timer, int(config["delay"] * 1000), loops=config["bursts"]
             )
             self.next_wave_in = None
-            self.level.logger.debug(
+            self.game.logger.debug(
                 f"Starting burst wave {self._index} in {config['delay']} seconds"
             )
             self._burst_counter = 0
 
     def handle_burst(self):
         config = self._data[self._index]
-        self.level.logger.debug(
+        self.game.logger.debug(
             f"Starting burst {self._burst_counter} of {config['bursts']} from wave {self._index}"
         )
         for enemy, count in config["enemies"].items():
             for i in range(count):
                 match enemy:
                     case "slime":
-                        self.level.enemies_group.add(catwars.enemies.Slime(self.level))
+                        self.game.enemies_group.add(catwars.enemies.Slime(self.game))
                     case "bat":
-                        self.level.enemies_group.add(catwars.enemies.Bat(self.level))
+                        self.game.enemies_group.add(catwars.enemies.Bat(self.game))
                     case "ghost":
-                        self.level.enemies_group.add(catwars.enemies.Ghost(self.level))
+                        self.game.enemies_group.add(catwars.enemies.Ghost(self.game))
                     case _:
                         raise Exception("Unsupported enemy type in wave config")
         self._burst_counter += 1
